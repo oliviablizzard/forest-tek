721a68436d46f335743f7cbdaa514bc5855e5174
Olivia Blizzard <olivialblizzard@gmail.com>
2024-10-17 16:09:10 -0300

    format basic header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,17 @@
+import './Header.scss';
+import Logo from '../../assets/icons/logo.png';
+import Menu from '../../assets/icons/menu.png'
+
 export default function Header() {
     return (
-        <>
-        <h1>Header</h1>
-        </>
+        <header className="wrapper header">
+            <div className="wrapper">
+                <img src={Logo} className="logo" />
+                <h1 className="logo__name">Forest-TEK</h1>
+            </div>
+            <div>
+                <img src={Menu} className="menu" />
+            </div>
+        </header>
     );
 }
